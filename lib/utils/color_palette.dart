b37228d15f63838409e892316bad2ae4c4cb0b6c
Asik Azad <47155662+AsikAzad@users.noreply.github.com>
2023-06-28 16:22:06 +0600

    Project config files

---
+++ lib/utils/color_palette.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const Color iconColor = Color(0xFFB6C7D1);
+  static const Color activeColor = Color.fromARGB(255, 10, 130, 102);
+  static const Color textColor1 = Color(0XFFA7BCC7);
+  static const Color textColor2 = Color(0XFF9BB3C0);
+  static const Color lightCyan = Color.fromARGB(255, 71, 146, 158);
+  static const Color yellowColor = Color.fromARGB(255, 248, 215, 26);
+  static const Color whiteColor = Colors.white;
+  static const Color orangeColor = Colors.orange;
+  static Color shadowColor = Colors.black.withOpacity(0.3);
+  static const Color facebookColor = Color(0xFF3b5998);
+  static const Color googleColor = Color(0xFFDE4B39);
+  static const Color backgroundColor = Color(0xFFECF3F9);
+}
---
+++ lib/utils/size_config.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  static late MediaQueryData _mediaQueryData;
+  static late double screenWidth;
+  static late double screenHeight;
+  static late double scaleFactor;
+  static late double defaultSize;
+  static late Orientation orientation;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    scaleFactor = _mediaQueryData.textScaleFactor;
+    orientation = _mediaQueryData.orientation;
+    // On iPhone 11 the defaultSize = 10 almost
+    // So if the screen size increase or decrease then our defaultSize also vary
+    defaultSize = orientation == Orientation.landscape
+        ? screenHeight * 0.024 * (1.0 / scaleFactor)
+        : screenWidth * 0.024 * (1.0 / scaleFactor);
+  }
+}
